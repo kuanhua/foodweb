7843649832685b0827a7e6cb002aa002baba0ff5
kuanhua <kuan1022@hotmail.com>
2016-08-13 04:38:18 +0800

    2016.08.13 AM 4:38

--- foodweb/settings.py
+++ foodweb/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    # add by Kuanhua
+    'django.middleware.locale.LocaleMiddleware',
+    #
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
