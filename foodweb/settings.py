8f8ce3a447e6a6346e8bf22fcca883849ea43b74
kuanhua <kuan1022@hotmail.com>
2016-08-15 04:45:36 +0800

    2016.08.15 AM 4:45

--- foodweb/settings.py
+++ foodweb/settings.py
@@ -47,11 +47,12 @@ MIDDLEWARE = [
     'django.middleware.locale.LocaleMiddleware',
     #
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+# 'django.middleware.csrf.CsrfViewMiddleware',
 
 ROOT_URLCONF = 'foodweb.urls'
 
@@ -117,8 +118,12 @@ USE_L10N = True
 
 USE_TZ = True
 
+##CSRF_COOKIE_SECURE = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,8 +1,10 @@
 #-*- coding: UTF-8 -*-
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+##from django.template.context_processors import csrf
 from django.shortcuts import HttpResponseRedirect
-from restaurants.models import Restaurant
+from restaurants.models import Restaurant,  Comment
+from restaurants.forms import CommentForm
 from django.utils import timezone
 
 
@@ -12,7 +14,7 @@ from django.utils import timezone
 def menu(request):
     if 'id' in request.GET and request.GET['id'] != '':
         restaurant = Restaurant.objects.get(id=request.GET['id'])
-        return render_to_response('menu.html', RequestContext(request, locals()))
+        return render_to_response('menu.html', locals())
     else:
         return HttpResponseRedirect("/restaurants_list")
 
@@ -30,18 +32,46 @@ def menu(request,id):
 '''
 def list_restaurants(request):
     restaurants = Restaurant.objects.all()
-    return render_to_response('restaurants_list.html', RequestContext(request, locals()))
+    return render_to_response('restaurants_list.html', locals())
 
 def comment(request,restaurant_id):
+
     if restaurant_id:
         r = Restaurant.objects.get(id=restaurant_id)
     else:
-        return HttpResponseRedirect("/restaurants_list")
+        return HttpResponseRedirect("/restaurants_list/")
+    if request.POST:
+        f = CommentForm(request.POST)
+        if f.is_valid():
+            visitor = request.POST['visitor']
+            content = request.POST['content']
+            email = request.POST['email']
+            date_time = timezone.localtime(timezone.now())
+            c = Comment.objects.create(
+                visitor=visitor,
+                email=email,
+                content=content,
+                date_time=date_time,
+                restaurant=r
+            )
+            f = CommentForm(initial={'content':'沒意見'})
+    else:
+        f = CommentForm(initial={'content':'沒意見'})
+    return render_to_response('comments.html',RequestContext(request,locals()))
+'''
+自行驗證方式
+    errors = []
     if request.POST:
         visitor = request.POST['visitor']
         content = request.POST['content']
         email = request.POST['email']
         date_time = timezone.localtime(timezone.now())
-        Comment.objects.create(visitor=visitor,email=email,content=content,date_time=date_time,restaurant=r)
-##  return render_to_response('comments.html',locals())
-    return render_to_response('comments.html', RequestContext(request, locals()))
+        if any(not request.POST[k] for k in request.POST):
+            errors.append('* 有空白欄位，請不要留空')
+        if '@' not in email:
+            errors.append('* email格式不正確，請重新輸入')
+        if not errors:
+            Comment.objects.create(visitor=visitor,email=email,content=content,date_time=date_time,restaurant=r)
+            visitor,email,content=('','','')
+'''
+
